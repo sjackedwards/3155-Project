88df56aff281b539e7082bb9a0fcf7267f11ed10
Sam Edwards <sjackedwards@gmail.com>
2023-04-05 21:14:30 -0400

    File tree changes, initial commit of more .puml

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-DB_FILE = 'local_app.db'
-
 # Credit to my co-worker who greatly helped me set this database up.
 def setup_database():
-    conn = sqlite3.connect(DB_FILE)
+    conn = sqlite3.connect('local_app.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users
                  (username TEXT PRIMARY KEY, password TEXT)''')
@@ -13,7 +11,7 @@ def setup_database():
 
 # For the registration_test to run multiple times.
 def cleanup_test_user():
-    conn = sqlite3.connect(DB_FILE)
+    conn = sqlite3.connect('local_app.db')
     c = conn.cursor()
     c.execute("DELETE FROM users WHERE username = 'testuser'")
     conn.commit()
