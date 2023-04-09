e3ee2ea0bdabd8a2dd9ca2cbe87f3cf6e208ac2b
Sam Edwards <sjackedwards@gmail.com>
2023-04-08 21:37:44 -0400

    GUI overhaul backup

--- core_gui.py
+++ core_gui.py
@@ -7,66 +7,91 @@ class Core(tk.Toplevel):
         super().__init__(master)
 
         self.title("Flight Searcher")
-        self.geometry("1100x600")
+        self.geometry("1000x650")
         self.username = username
         self.api_key = api_key
 
         self.create_widgets()
+
+        for i in range(15):
+            self.columnconfigure(i, weight=15)
 
 
     # TODO 1: Clean literally everything up. Lets make a second listbox for return flights instead of lumping them all into one.
     # TODO 2: I dont think I would like to book a flight that is leaving 45 mins after mine lands, lets implement a sort by time button.
     # TODO 3: Change some button names, think of some new buttons we can add, maybe sort buttons. Need to look into tkinter capabilities. 
     def create_widgets(self):
+        # Welcome label
         self.lbl_username = tk.Label(self, text=f"Welcome, {self.username}")
-        self.lbl_username.grid(row=0, column=0, padx=5, pady=5)
-
-        self.carrier_var = tk.StringVar(self)
-        self.carrier_var.set("Any Airline")
-        self.carrier_menu = tk.OptionMenu(self, self.carrier_var, "Any Airline", "Southwest", "Delta", "United Airlines", "jetBlue", "American Airlines", "SPIRIT")
-        self.carrier_menu.config(width=20)
-        self.carrier_menu.grid(row=1, column=0, padx=5, pady=5)
-
-        self.lbl_start_date = tk.Label(self, text="Depart Date:")
-        self.lbl_start_date.grid(row=2, column=0, padx=5, pady=5)
-        self.start_date = DateEntry(self, date_pattern='yyyy-mm-dd')
-        self.start_date.grid(row=2, column=1, padx=5, pady=5)
-
-        self.lbl_end_date = tk.Label(self, text="Return Date:")
-        self.lbl_end_date.grid(row=3, column=0, padx=5, pady=5)
-        self.end_date = DateEntry(self, date_pattern='yyyy-mm-dd')
-        self.end_date.grid(row=3, column=1, padx=5, pady=5)
-
-        self.lbl_departing_airport = tk.Label(self, text="Departing Airport:")
-        self.lbl_departing_airport.grid(row=2, column=2, padx=5, pady=5)
-        self.departing_airport_var = tk.StringVar()
-        self.entry_departing_airport = tk.Entry(self, textvariable=self.departing_airport_var)
-        self.entry_departing_airport.grid(row=2, column=3)
-
-        self.lbl_arriving_airport = tk.Label(self, text="Arriving Airport:")
-        self.lbl_arriving_airport.grid(row=3, column=2, padx=5, pady=5)
-        self.arriving_airport_var = tk.StringVar()
-        self.entry_arriving_airport = tk.Entry(self, textvariable=self.arriving_airport_var)
-        self.entry_arriving_airport.grid(row=3, column=3)
-
-        self.flight_listbox = tk.Listbox(self, height=20)
-        self.flight_listbox.config(width=80)
-        self.flight_listbox.grid(row=6, column=0, pady=5)
-
-        self.scrollbar = tk.Scrollbar(self, command=self.flight_listbox.yview)
-        self.scrollbar.grid(row=6, column=2, sticky='ns')
-        self.flight_listbox.config(yscrollcommand=self.scrollbar.set)
-
-
-
-        self.submit_button = tk.Button(self, text="Submit", command=self.submit)
-        self.submit_button.grid(row=8, column=0, padx=5, pady=5)
-
-        self.exit_button = tk.Button(self, text="Exit", command=self.close_app)
-        self.exit_button.grid(row=8, column=1, padx=5, pady=5)
+        self.lbl_username.place(x=200, y=10)
 
         self.lbl_api_key = tk.Label(self, text=f"API KEY loaded:  {self.api_key}")
-        self.lbl_api_key.grid(row=9, column=1, padx=5, pady=5)
+        self.lbl_api_key.place(x=400, y=10)
+
+        # Carrier dropdown menu
+        self.carrier_var = tk.StringVar(self)
+        self.carrier_var.set("Any Airline")
+        self.carrier_menu = tk.OptionMenu(self, self.carrier_var, "Any Airline", "Southwest", "Delta", "United Airlines", "jetBlue", "American Airlines", "Spirit")
+        self.carrier_menu.config(width=22)
+        self.carrier_menu.place(x=25, y=45)
+
+        # Departure date label and input
+        self.lbl_start_date = tk.Label(self, text="Depart Date:")
+        self.lbl_start_date.place(x=25, y=85)
+        self.start_date = DateEntry(self, date_pattern='yyyy-mm-dd')
+        self.start_date.place(x=105, y=85)
+
+
+        # Return date label and input
+        self.lbl_end_date = tk.Label(self, text="Return Date:")
+        self.lbl_end_date.place(x=25, y=110)
+        self.end_date = DateEntry(self, date_pattern='yyyy-mm-dd')
+        self.end_date.place(x=105, y=110)
+
+
+
+
+        # Departing airport label and input
+        self.lbl_departing_airport = tk.Label(self, text="Departing Airport:")
+        self.lbl_departing_airport.place(x=235, y=85)
+        self.departing_airport_var = tk.StringVar()
+        self.entry_departing_airport = tk.Entry(self, textvariable=self.departing_airport_var)
+        self.entry_departing_airport.config(width= 10)
+        self.entry_departing_airport.place(x=340, y=85)
+
+        # Arriving airport label and input
+        self.lbl_arriving_airport = tk.Label(self, text="Arriving Airport:")
+        self.lbl_arriving_airport.place(x=235, y=110)
+        self.arriving_airport_var = tk.StringVar()
+        self.entry_arriving_airport = tk.Entry(self, textvariable=self.arriving_airport_var)
+        self.entry_arriving_airport.config(width= 10)
+        self.entry_arriving_airport.place(x=340, y=110)
+
+        # # Flight listbox with scrollbar
+        # self.flight_listbox = tk.Listbox(self, height=20, width=40)
+        # self.flight_listbox.grid(row=4, column=0, padx=5, pady=5, columnspan=2, sticky='nsew')
+        # self.scrollbar = tk.Scrollbar(self, command=self.flight_listbox.yview)
+        # self.scrollbar.grid(row=4, column=2, sticky='ns', pady=5)
+        # self.flight_listbox.config(yscrollcommand=self.scrollbar.set)
+
+        # # Return flight listbox with scrollbar
+        # self.ret_flight_listbox = tk.Listbox(self, height=20, width=40)
+        # self.ret_flight_listbox.grid(row=4, column=3, padx=5, pady=5, columnspan=2, sticky='nsew')
+        # self.ret_scrollbar = tk.Scrollbar(self, command=self.ret_flight_listbox.yview)
+        # self.ret_scrollbar.grid(row=4, column=5, sticky='ns', pady=5)
+        # self.ret_flight_listbox.config(yscrollcommand=self.ret_scrollbar.set)
+
+
+
+        # # Submit button
+        # self.submit_button = tk.Button(self, text="Submit", command=self.submit)
+        # self.submit_button.grid(row=5, column=0, padx=5, pady=5)
+
+        # Exit button
+        self.exit_button = tk.Button(self, text="Exit", command=self.close_app)
+        self.exit_button.place(x=250, y=550)
+
+        
 
     def close_app(self):
         quit()
@@ -140,3 +165,8 @@ class Core(tk.Toplevel):
                         self.flight_listbox.insert(tk.END, f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    app = Core(root, username="Demo", api_key="DEMO_API_KEY")
+    root.mainloop()
